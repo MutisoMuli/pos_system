cca6af5c28f65dbf64c749bf56aba3483d919d57
Astonke <125920648+Astonke@users.noreply.github.com>
2024-10-08 09:53:47 -0700

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product-list'),
+    path('', views.ProductListView.get_context_data, name='product-list'),
     path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
-    path('product/new/', views.ProductCreateView.as_view(), name='product-create'),
+    path('product/new/', views.ProductCreateView.form_page, name='product-create'),
     path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product-update'),
     path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product-delete'),
 ]
